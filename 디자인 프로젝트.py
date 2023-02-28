9ea02e66c816da77e5df97244c56b59721c0d8e1
Cheolwoo-Park <74113650+Cheolwoo-Park@users.noreply.github.com>
2023-02-27 23:09:55 -0500

    Update 디자인 프로젝트.py

--- 디자인 프로젝트.py
+++ 디자인 프로젝트.py
@@ -16,10 +16,13 @@ def getHTML_bs(url):
         bs=BeautifulSoup(html.read(),'html.parser')
         return bs
 
-bs=getHTML_bs('http://www.pythonscraping.com/pages/warandpeace.html')
+bs=getHTML_bs('https://www.pythonscraping.com/pages/page3.html')
 #attributeerror 예외처리 필요
 
-#print(bs.get_text())
+'''
+find(),find_all() 함수 공부.
+
+print(bs.get_text())
 #.get_text()함수는 문서 내의 모든 html 태그들을 제거하고, 텍스트들만 유니코드로 반환함.
 
 namelist=bs.findAll('span',{'class':'green'})
@@ -34,24 +37,29 @@ list_recursion=bs.find_all('span',{'class':'red'},False)
 #recursive 인수가 True이면, 자식 태그들까지 조건에 맞는 태그들을 모두 찾음, False이면 가장 상위의 태그만 찾음.
 
 list_prince=bs.find_all(string='the prince')
-print(len(list_prince))
 #string 인수는 태그의 속성이 아니라 태그에 의해 감싸진 텍스트 값을 통해 태그들을 필터링함.
 
 list_limit=bs.find_all('span',limit=5)
-'''
-limit 인수는 페이지의 처음부터 해당 태그들 x개를 찾을때 이용함, find함수는 find_all함수의 limit 인수가 1임(인수로 입력받은 조건에 해당하는,
-페이지 내에서 가장 처음으로 나타나는 태그 1개만 찾아줌.)
-'''
+#limit 인수는 페이지의 처음부터 해당 태그들 x개를 찾을때 이용함, find함수는 find_all함수의 limit 인수가 1임(인수로 입력받은 조건에 해당하는,
+#페이지 내에서 가장 처음으로 나타나는 태그 1개만 찾아줌.)
+
 
 list_keyword=bs.find_all(class_='red')
-'''
-keyword 인수는 어떤 속성 값이 특정한 값을 가진 태그들을 찾을 때 이용함. 원하는 속성 값들을 딕셔너리로 인수로
-전달하면 or조건이, keyword는 and 조건이 적용되어 태그들이 필터링됨.
+#keyword 인수는 어떤 속성 값이 특정한 값을 가진 태그들을 찾을 때 이용함. 원하는 속성 값들을 딕셔너리로 인수로
+#전달하면 or조건이, keyword는 and 조건이 적용되어 태그들이 필터링됨.
 '''
 
 
 
 '''
-for child in bs.find('table',{'id':'giftList'}).tr.next_siblings:
-    print(child)
+beautifulsoup로 DOM tree 이용하기.
+
+table_row_list=bs.find('table',{'id':'giftList'}).children
+for a in table_row_list:
+    print(a)
+#beautifulsoup는 현재 선택된 태그들을 중심으로 자손(descendant) 태그들을 찾기 때문에 child 태그들만 찾으려면 .children를 써야됨.
+#a.tag.subtag를 실행하면, tag밑의 자손 태그들 중에 가장 처음에 나타나는 subtag 1개만 선택됨.(child ∈ descendant)
 '''
+
+for b in bs.find('table',{'id':'giftList'}).tr.next_siblings:
+    print(b)
